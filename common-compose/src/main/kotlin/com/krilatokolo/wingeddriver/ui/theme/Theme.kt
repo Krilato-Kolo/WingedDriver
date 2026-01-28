37f44ec4ce05bf476457db038274b87141eee3fe
Matej Drobnič <matej@matejdro.com>
2026-01-28 14:41:43 +0100

    feat: add theme and icons

--- common-compose/src/main/kotlin/com/krilatokolo/wingeddriver/ui/theme/Theme.kt
+++ common-compose/src/main/kotlin/com/krilatokolo/wingeddriver/ui/theme/Theme.kt
@@ -13,29 +13,19 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-   primary = Purple80,
-   secondary = PurpleGrey80,
-   tertiary = Pink80
+   primary = Color(0xffaac7ff),
+   secondary = Color(0xffbec6dc),
+   tertiary = Color(0xffddbce0)
 )
 
 private val LightColorScheme = lightColorScheme(
-   primary = Purple40,
-   secondary = PurpleGrey40,
-   tertiary = Pink40
-
-   /*
-      Other default colors to override
-      background = Color(0xFFFFFBFE),
-      surface = Color(0xFFFFFBFE),
-      onPrimary = Color.White,
-      onSecondary = Color.White,
-      onTertiary = Color.White,
-      onBackground = Color(0xFF1C1B1F),
-      onSurface = Color(0xFF1C1B1F),
-    */
+   primary = Color(0xff415f91),
+   secondary = Color(0xff565f71),
+   tertiary = Color(0xFF705575)
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
